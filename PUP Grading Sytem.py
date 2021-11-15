6253943844812c085b98f1e442334cf2f527f9fa
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-15 18:26:30 +0800

    Second condition

--- PUP Grading Sytem.py
+++ PUP Grading Sytem.py
@@ -4,3 +4,6 @@ def header():
 if gradeP >= 97 and gradeP <= 100:
         print("Grade/Mark: 1.00")
         print(f"Description: {status[1]}")
+elif gradeP >= 94 and gradeP <= 96:
+    print("Grade/Mark: 1.25")
+    print(f"Description: {status[1]}")
