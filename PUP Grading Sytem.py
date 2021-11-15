ed1c26b8af3c33f1cda1e1b043841bc2b119ed06
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-15 18:29:10 +0800

    Fourth condition

--- PUP Grading Sytem.py
+++ PUP Grading Sytem.py
@@ -10,3 +10,6 @@ elif gradeP >= 94 and gradeP <= 96:
 elif gradeP >= 91 and gradeP <= 93:
     print("Grade/Mark: 1.5")
     print(f"Description: {status[2]}")
+elif gradeP >= 88 and gradeP <= 90:
+    print("Grade/Mark: 1.75")
+    print(f"Description: {status[2]}")
