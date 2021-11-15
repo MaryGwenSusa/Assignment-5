cba36b14e6852363ea5b3bc761e21c2598f42381
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-15 18:25:29 +0800

    First condition

--- PUP Grading Sytem.py
+++ PUP Grading Sytem.py
@@ -1,2 +1,6 @@
 def header():
     print(f"Polytechnic University of the Philippines (PUP) Grading System")
+
+if gradeP >= 97 and gradeP <= 100:
+        print("Grade/Mark: 1.00")
+        print(f"Description: {status[1]}")
