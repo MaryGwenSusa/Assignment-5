b528931b2133f8bcf46bf0eb7852d7ab5d19a82e
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-16 14:43:56 +0800

    All inputs in one function

--- Lowest.py
+++ Lowest.py
@@ -1,6 +1,8 @@
 def lowest():
     print(f"Which is the lowest?")
 
-firstNumber = float(input("First Number: \n >"))
-secondNumber = float(input("Second Number: \n >"))
-thirdNumber = float(input("Third NUmber: \n >"))
+def numbers():
+    firstNumber = float(input("First Number: \n >"))
+    secondNumber = float(input("Second Number: \n >"))
+    thirdNumber = float(input("Third NUmber: \n >"))
+    return firstNumber, secondNumber, thirdNumber
