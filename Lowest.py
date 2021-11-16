1de2d2bb41d81622b5d86fece1c00a9bb3b5b1f1
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-16 14:53:14 +0800

    Another version with logical operators

--- Lowest.py
+++ Lowest.py
@@ -17,3 +17,25 @@ elif secondGiven > thirdGiven <= firstGiven:
     print(f"The lowest number is {thirdGiven}")
 elif firstGiven == secondGiven == thirdGiven:
     print("The numbers are equal.")
+
+
+# another version
+def lowest():
+    print(f"Which is the lowest?")
+
+def numbers():
+    firstNumber = float(input("First Number: \n >"))
+    secondNumber = float(input("Second Number: \n >"))
+    thirdNumber = float(input("Third NUmber: \n >"))
+    return firstNumber, secondNumber, thirdNumber
+
+lowest()
+firstGiven, secondGiven, thirdGiven = numbers()
+if firstGiven <= secondGiven and firstGiven < thirdGiven:
+    print(f"The lowest number is {firstGiven}.")
+elif secondGiven < firstGiven and secondGiven <= thirdGiven:
+    print(f"The lowest number is {secondGiven}.")
+elif thirdGiven <= firstGiven and thirdGiven < secondGiven:
+    print(f"The lowest number is {thirdGiven}")
+elif firstGiven == secondGiven == thirdGiven:
+    print("The numbers are equal.")
